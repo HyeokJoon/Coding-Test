4f39233c780d8403cc577dc6f2414d855898f247
Hyeok <fufu98@naver.com>
2022-10-08 19:25:56 +0900

    measure

--- 0.cpp
+++ 0.cpp
@@ -3,10 +3,16 @@
 
 using namespace std;
 
-vector<int> solution(int money)
+vector<int> solution(int n)
 {
     vector<int> answer;
-    answer.push_back(money / 5500);
-    answer.push_back(money % 5500);
+    answer.push_back(1);
+    for (int i = 2; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            answer.push_back(i);
+        }
+    }
     return answer;
 }
