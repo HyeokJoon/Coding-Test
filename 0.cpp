e87ed98e14033506e5dc61147cebedd061ad2435
Hyeok <fufu98@naver.com>
2022-10-11 21:46:23 +0900

    배열 자르기

--- 0.cpp
+++ 0.cpp
@@ -1,13 +1,14 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-vector<int> solution(vector<int> num_list)
+vector<int> solution(vector<int> numbers, int num1, int num2)
 {
     vector<int> answer;
-    reverse(num_list.begin(), num_list.end());
-
-    return num_list;
+    for (num1; num1 <= num2; num1++)
+    {
+        answer.push_back(numbers[num1]);
+    }
+    return answer;
 }
