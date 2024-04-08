715426fc6480c27efcc0812b09990946d6b75b0b
HyeokJoon <87688726+HyeokJoon@users.noreply.github.com>
2024-04-08 11:19:34 +0900

    [Silver I] Title: 회의실 배정, Time: 4220 ms, Memory: 52032 KB -BaekjoonHub

---
+++ Python/백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,15 @@
+n = int(input())
+meetings = [tuple(map(int, input().split())) for _ in range(n)]
+
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+# print(meetings)
+cnt = 0
+end = 0
+for i in range(n):
+    if meetings[i][0] < end:
+        continue
+    cnt += 1
+    end = meetings[i][1]
+
+print(cnt)
